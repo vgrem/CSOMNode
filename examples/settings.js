89831bf58a2d66e92edda32dc5619b6e55a979b0
Vadim Gremyachev <vvgrem@gmail.com>
2016-01-29 14:40:11 +0200

    User Profile API has been added

--- examples/settings.js
+++ examples/settings.js
@@ -1,8 +1,8 @@
 ﻿var settings = {
-    tenantUrl: "https://tenant.sharepoint.com",
-    webUrl: "/",  //Note:for root web set url to '/'
-    username: "username@tenant.onmicrosoft.com",
-    password: "password"
+    tenantUrl: "https://media16.sharepoint.com",
+    webUrl: "/sites/news",  //Note:for root web set url to '/'
+    username: "vgrem@media16.onmicrosoft.com",
+    password: "***REMOVED***"
 };
 
 exports.settings = settings;
